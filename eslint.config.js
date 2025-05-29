9ff4dadf94c40f78a694e49b5f307bc578d8dc98
Liry <learainccc@outlook.com>
2025-05-29 17:05:41 +0800

    增加配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
 import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-const eslintConfig = [
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
+const eslintConfig = defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser }
+  },
   tseslint.configs.recommended
-];
+]);
 
 export default eslintConfig;
