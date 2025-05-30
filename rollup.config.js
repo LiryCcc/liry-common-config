44e1fa82af6bcbd58e074cb630379086940d29d2
Liry <learainccc@outlook.com>
2025-05-30 09:46:20 +0800

    改变配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,15 @@ import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 import clear from 'rollup-plugin-clear';
 // 如果需要压缩 UMD/IIFE 输出，可以添加
-// import terser from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const outDir = 'dist';
+const name = 'LiryCommonConfig';
 
 const rollupConfig = defineConfig({
   input: 'src/index.ts',
   output: [
+    // 已有的格式
     {
       dir: outDir,
       format: 'cjs',
@@ -26,8 +28,47 @@ const rollupConfig = defineConfig({
     {
       dir: outDir,
       format: 'umd',
-      name: 'LiryCommonConfig',
+      name,
       entryFileNames: 'index.umd.js',
+      sourcemap: true
+    },
+
+    // 新增格式
+    {
+      dir: outDir,
+      format: 'iife',
+      name,
+      entryFileNames: 'index.iife.js',
+      sourcemap: true
+    },
+    {
+      dir: outDir,
+      format: 'amd',
+      entryFileNames: 'index.amd.js',
+      sourcemap: true
+    },
+    {
+      dir: outDir,
+      format: 'system',
+      entryFileNames: 'index.system.js',
+      sourcemap: true
+    },
+
+    // 可选：添加压缩版本
+    {
+      dir: outDir,
+      format: 'umd',
+      name,
+      entryFileNames: 'index.umd.min.js',
+      plugins: [terser()],
+      sourcemap: true
+    },
+    {
+      dir: outDir,
+      format: 'iife',
+      name,
+      entryFileNames: 'index.iife.min.js',
+      plugins: [terser()],
       sourcemap: true
     }
   ],
@@ -41,7 +82,8 @@ const rollupConfig = defineConfig({
       declarationDir: outDir
     }),
     commonjs(),
-    resolve()
+    resolve(),
+    terser()
   ]
 });
 
